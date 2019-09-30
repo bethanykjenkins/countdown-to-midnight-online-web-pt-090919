957bc1619682682c0bb476b4092b1575e4f340e2
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-30 16:36:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,14 @@ def countdown
 puts "HAPPY NEW YEAR!"  
 end
 countdown
+
+def countdown_with_sleep
+number = 10 
+while number >= 0
+puts "#{number} SECOND(S)!"
+number = number - 1
+sleep (1) 
+end
+puts "HAPPY NEW YEAR!"
+end 
+countdown_with_sleep
